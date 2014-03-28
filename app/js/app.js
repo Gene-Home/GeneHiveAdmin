fff9f481f36bb7f49ae09dd225e91f6c2dd6f3ec
pshaughn <pshaughn@comcast.net>
2014-03-28 13:17:44 -0400

    Avoid double-loading controller

--- app/js/app.js
+++ app/js/app.js
@@ -14,7 +14,7 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/jobRuns', {templateUrl: 'partials/jobRuns.html', controller: 'JobRunListCtrl'});
+  $routeProvider.when('/jobRuns', {templateUrl: 'partials/jobRuns.html'});
   $routeProvider.when('/jobRuns/:jobRunId', {templateUrl: 'partials/jobRunDetails.html', controller: 'JobRunDetailsCtrl'});	
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
