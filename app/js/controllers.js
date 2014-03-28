49f1c52ee59788feb479e63e64374c32631d8056
pshaughn <pshaughn@comcast.net>
2014-03-28 14:33:50 -0400

    Sort is now called external to grid so it takes priority over slice

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,7 @@
 angular.module('myApp.controllers', []);
 
 var geneHiveControllers = angular.module('geneHiveControllers', []); 
-geneHiveControllers.controller('JobRunListCtrl', ['$scope','$http', function($scope,$http) {
+geneHiveControllers.controller('JobRunListCtrl', ['$scope','$http','$sortService', function($scope,$http,$sortService) {
     $scope.cachedServerData = null;
     $scope.refreshData=function() {
 	$scope.cachedServerData = null;
@@ -14,7 +14,7 @@ geneHiveControllers.controller('JobRunListCtrl', ['$scope','$http', function($sc
         }
 	$http.get('/GeneHive/api/v2/JobRuns/').success(function (data) {
 	    $scope.cachedServerData = data;
-	    $scope.setPagingData($scope.pagingOptions.currentPage,$scope.pagingOptions.pageSize);
+	    $scope.setPagingData();
 	})};
     $scope.totalServerItems = 0;
     $scope.pagingOptions = {
@@ -22,26 +22,41 @@ geneHiveControllers.controller('JobRunListCtrl', ['$scope','$http', function($sc
         pageSize: 250,
         currentPage: 1
     };	
-    $scope.setPagingData = function(page, pageSize){	
+    $scope.sortInfo = { fields: [], columns: [], directions: [] }
+    $scope.setPagingData = function(page, pageSize){
+	if($scope.cachedServerData==null)
+	{
+	    return;
+	}
+	var page = $scope.pagingOptions.currentPage;
+	var pageSize = $scope.pagingOptions.pageSize;
+	$sortService.Sort($scope.sortInfo,$scope.cachedServerData);
         var pagedData = $scope.cachedServerData.slice((page - 1) * pageSize, page * pageSize);
         $scope.myData = pagedData;
-        $scope.totalServerItems = data.length;
+        $scope.totalServerItems = $scope.cachedServerData.length;
         if (!$scope.$$phase) {
             $scope.$apply();
         }
     };
     $scope.$watch('pagingOptions', function (newVal, oldVal) {
-        if (newVal !== oldVal) {
-          $scope.setPagingData($scope.pagingOptions.currentPage, $scope.pagingOptions.pageSize);
-        }
+        $scope.setPagingData();
     }, true);
+    $scope.$watch('sortInfo', function(newVal, oldVal) {
+	console.log("sortInfo watch fired");
+	$scope.setPagingData();
+    }, true);
+    $scope.$on('ngGridEventSorted', function(newSort) {
+	console.log("grid sort event fired");
+    });
 
 $scope.selectedJobRun = [];	
     $scope.gridOptions = {
         data: 'myData',
         enablePaging: true,
 		showFooter: true,
+	useExternalSorting: true,
         totalServerItems: 'totalServerItems',
+	sortInfo: $scope.sortInfo,
         pagingOptions: $scope.pagingOptions,
         filterOptions: $scope.filterOptions,
         columnDefs: [{field:'id', displayName:'ID',width: 30},
@@ -50,7 +65,6 @@ $scope.selectedJobRun = [];
         			  {field:'jobType', displayName:'Job Type'},
                      {field:'runDatetime',displayName:'Run Date'}, 
                      {field:'status',displayName:'Status',cellTemplate: '<div ng-class="{green: row.getProperty(col.field) ==\'COMPLETE\'}"><div class="ngCellText">{{row.getProperty(col.field)}}</div></div>'}],
-        
         multiSelect: false,
         selectedItems: $scope.selectedJobRun
     };
