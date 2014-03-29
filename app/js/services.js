54d110dd137291dfc302ea426abedd6b5fca3ee7
David Ulrich <ghdulrich@gmail.com>
2014-03-29 16:06:15 -0400

    added user,workfile and group services

--- app/js/services.js
+++ app/js/services.js
@@ -8,6 +8,32 @@ angular.module('myApp.services', []).
   value('version', '0.1');
 
 var geneHiveServices = angular.module('geneHiveServices', ['ngResource']);
+
+geneHiveServices.factory('WorkFile',['$resource',
+  function($resource){
+    return $resource('/GeneHive/api/v2/WorkFileProperties/', {}, {
+      query: {method:'GET', isArray:true}
+    });
+  }]
+ );
+
+geneHiveServices.factory('UserGroup',['$resource',
+  function($resource){
+    return $resource('/GeneHive/api/v2/Groups/', {}, {
+      query: {method:'GET', isArray:true}
+    });
+  }]
+ );
+
+geneHiveServices.factory('User',['$resource',
+  function($resource){
+    return $resource('/GeneHive/api/v2/Users/:uname', {}, {
+      query: {method:'GET', isArray:true},
+      update: {method:'PUT'},
+      create: {method:'POST'}
+    });
+  }]
+ );
  
 geneHiveServices.factory('JobRun', ['$resource',
   function($resource){
