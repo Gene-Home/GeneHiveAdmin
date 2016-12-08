6018b29f34c0ae33f6ec97823747a924a2ea4e29
David Ulrich <ghdulrich@gmail.com>
2016-12-08 18:02:49 -0500

    forgot to add the login_controller

---
+++ app/js/login_controller.js
@@ -0,0 +1,49 @@
+
+var loginController = angular.module('geneHive.LoginController',[]);
+
+loginController.controller('LoginCtrl',['$scope','$rootScope','$http','$state','AuthService',
+   function ($scope,$rootScope,$http,$state,AuthService) {
+
+     
+      $scope.reminding = false;
+      $scope.login = function(){
+    	 var uname = $scope.userName;
+    	 var password = $scope.password;
+       $scope.bad_login = false;
+
+    	 AuthService.login(uname,password)
+    		  .then(function success(data) {
+            // show the users profile
+            $scope.bad_login = false;
+            $state.go('index');
+  					console.log('Success!', data);
+
+			 }, function error(msg) {
+            // let him know that he does not have one
+            // if the code is 2 
+            // otherwise something is broken 
+  		  		console.error('Login Failure!', msg);
+            $scope.bad_login = true;
+			 });
+    }// end login
+
+    $scope.remindPassword = function () {
+
+      AuthService.remindPassword($scope.email).then(
+        function(success){
+          alert('Email Sent');
+          $scope.reminding = false;
+        },function(fail){
+          alert("Problem " + fail);
+      })
+    };// end createClass
+
+    $scope.logout = function(){
+        AuthService.logout();
+    }
+
+    if(! $state.current.data.logging_in){
+        $scope.logout();
+      } 
+      
+  }]);
